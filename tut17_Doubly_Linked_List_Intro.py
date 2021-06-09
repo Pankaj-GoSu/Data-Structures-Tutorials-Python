97c433e6fd5fe75bcec10900c9f4c2a69cb191d3
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-09 22:36:10 +0530

    Added Tut17 And Tut18

---
+++ tut17_Doubly_Linked_List_Intro.py
@@ -0,0 +1,17 @@
+#============ Introduction To Doubly Linked List ================
+
+'''
+Each Node Consist of 1 data and 2 Links: link of next node and link of previous node.
+
+Doubly Linked List : Prev Link , Data , Next Link.
+
+'''
+
+'''
+
+Doubly Linked List Operations:
+1- Insertion
+2- Deletion
+3- traversal
+
+'''
