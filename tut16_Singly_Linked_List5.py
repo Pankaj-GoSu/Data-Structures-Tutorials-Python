e00bd91f08f9b646697f88f455edc2fda9ecd1d8
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-09 12:18:31 +0530

    Added Tut16

--- tut16_Singly_Linked_List5.py
+++ tut16_Singly_Linked_List5.py
@@ -1,78 +1,14 @@
-#=========== Adding Elements after The given Node ============
+#=============== Deleting elements from Linked List =================
 
-
-#=========== First I write General Program =================
-'''
-
-  In This Program We Give index where we want to insert a node and we insert node at that point.
+# =========== Here I focused in concept so i take ideal cases not handel error =========
 
 '''
-'''
-
-class Node:
-
-    def __init__(self,data): #class constructor or initializtion method.
-        self.data = data
-        self.ref = None
-
-
-class LinkedList:
-
-    def __init__(self):
-        self.head = None # it means we do not have any node because head is None(Null),It is a Empty Linked List.
-
-    def print_LL(self): # Traversal Operation performed here
-        if self.head is None :
-            print("Linked List is empty!")
-        else:
-            n = self.head 
-            while n is not None :
-                print(n.data,"-->" , end =" ")
-                n = n.ref       
-
-    def add_begin(self,data):
-        new_node = Node(data)
-        new_node.ref = self.head
-        self.head = new_node
-
-    def add_end(self,data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-        
-        else:
-            n = self.head
-            while n.ref is not None :
-                n = n.ref
-
-            n.ref = new_node
-              
-    def in_Between_add(self,data,index): #index is where you want to insert node.
-        new_node = Node(data)
-        i = 1
-        n = self.head
-        while True:
-            if i == index - 1 :
-                new_node.ref = n.ref 
-                n.ref = new_node
-                break
-            else:
-                i += 1
-                n = n.ref
-    
-
-
-LL1 = LinkedList()
-LL1.add_begin(10)
-LL1.add_begin(20)
-LL1.in_Between_add(50,2)
-LL1.in_Between_add(50,4)
-LL1.add_end(100)
-LL1.add_end(30)
-LL1.print_LL()
+We can Delete Elemets at different places
+1- Begining --> del_begin()
+2- End --> del_end()
+3- In Between By Index and Value of node --> del_by_value(self,value)
 
 '''
-#========== Now Program for Adding Node after a perticular Node ================
 
 
 
@@ -174,6 +110,51 @@ class LinkedList:
                 j += 1
                 m = m.ref
 
+    def del_begin(self):
+        """ Deleted Begin node of Linked List"""
+        self.head =self.head.ref # directly We change head position so automatically Node deleted from Linked list
+
+    def del_end(self):
+        """ Deleted end Node of LinkedList"""
+        n = self.head
+        i = 1
+        while n.ref is not None: # this loop is for finding index of last element
+            n = n.ref
+            i +=1
+        j = 1
+        n = self.head
+        while True: # this loop run till 2nd last element and that we give refernce of 2nd last element to None
+            if j == i-1 :
+                n.ref = None
+                break
+            else:
+                j += 1
+                n = n.ref
+    
+    def del_by_value(self,value):
+        n = self.head # give value of pointer head to n so that we can traversal by n without cahnging head position.
+        i = 1
+        while n.ref is not None : # This loop is for index of that perticular node which value is given.
+            if n.data == value:
+                break
+            else:
+                n = n.ref
+                i += 1 
+        j = 1
+        m = self.head
+        while True:
+            if i == 1: # it means it is begin value of Linked List.
+                self.del_begin()
+            elif j == i-1: # here we change reference of node which is before the given node to next node of given node 
+                m.ref = n.ref 
+                break   
+            else:
+                j += 1
+                m = m.ref
+
+
+ 
+
 LL1 = LinkedList()
 LL1.add_begin(10)
 LL1.add_begin(20)
@@ -183,6 +164,9 @@ LL1.add_end(100)
 LL1.add_end(30)
 LL1.after_node(15,100) # enter 15 after value 100.
 LL1.before_node(25,20) # enter 25 before value 20.
+# LL1.del_begin()
+# LL1.del_end()
+LL1.del_by_value(25)
 LL1.print_LL()
 
 
