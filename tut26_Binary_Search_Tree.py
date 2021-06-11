edc9964621cfb960c745d51a55c6efde5f0b72ea
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-11 18:09:04 +0530

    Added tut26

---
+++ tut26_Binary_Search_Tree.py
@@ -0,0 +1,32 @@
+#=========== Binary Search Tree(BST) ===========
+
+'''
+Binary search tree is also called as ordered or sorted binary tree. it means binary search tree
+is a binary tree.
+
+'''
+
+#Binary Search tree is a binary tree with following propeties:
+
+# key's means value or node data
+
+#1- The left subtree of a node contains only nodes with keys lesser than node's key.
+
+#2- The right subtree of a node contains only nodes with keys greater than node's key.
+ 
+#3- The left subtree and right subtree each must also a BST.
+
+# THis above defination is for if all values are distinct ==========
+
+#============= Binary Search Tree With Duplicate values ==============
+
+'''
+1- In Some Books: Duplicate values not allowed in BST.
+2- In some Books: If Duplicate values are present than put these values on left side of node.
+3- In Some Books: If Duplicate values are present then put these values on right side of node.
+
+
+duplicate values are present at any level.
+
+And for duplicate value we make a counter which count how many time duplicate value occure.
+'''
