11054cab99b1a6492e4ad9cf0df684ea2f28cab5
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-11 12:42:23 +0530

    Added Tut24 and Tut25

---
+++ tut24_BinaryTree1.py
@@ -0,0 +1,22 @@
+# =========== Binary Tree 1 ============
+
+# ========== Full Binary Tree / Strictly Binary Tree  ==========
+
+'''
+Full Binary Tree is type of binary tree in which every node has 0 or 2 child nodes.
+or
+Full Binary Tree is a type of binary tree in which every node other then leaf nodes has 2 children 
+
+'''
+
+#============ Complete Binary Tree ======================
+
+'''
+It is a type of binary tree in which all the level are compeletly filled with nodes except last level.
+and in last level nodes filled completely or filled from left to right.
+
+Means:
+--> all the level except last level Completly filled with nodes
+--> last level it may be completly filled or nodes filled from left to right.
+
+'''
---
+++ tut25_BinaryTree2.py
@@ -0,0 +1,30 @@
+# ======= Here We Talk about remaing Binary Tree types ===========
+
+#========== Prefect Binary Tree ==============
+
+'''
+Type of binary tree in which all internal nodes contain 2 children and 
+all leaf nodes are present at same level 
+
+'''
+
+#========= Balanced Binary Tree ==============
+
+'''
+Balanced Binary Tree is a tree in which height of left and the right sub tree atmost differ by 1.
+
+Difference is absolute here.
+we have to find this differnce for every node of binary tree.
+
+# We take height of empty tree is -1  .
+
+'''
+
+# ========= Pathological Binary Tree / Degenrated Binary Tree ==========
+
+"""
+Degenrated binary tree is a binary tree where every parent node have only one child node.
+
+# That means every internal node contain only one child.
+
+"""
