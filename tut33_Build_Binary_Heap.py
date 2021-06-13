591924614b5af24675810a78eefcfaaa9aaa6a84
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-13 19:12:16 +0530

    Added some comment

--- tut33_Build_Binary_Heap.py
+++ tut33_Build_Binary_Heap.py
@@ -1,1 +1,15 @@
 #====== Create / Build Binary Heap(min heap or max heap) from List of Numbers ====== 
+
+# Here we see how to create binary heap tree when list of numbers id given.
+
+'''
+# 1st method:
+Take Empty Heap
+Insert elements of list to heap one by one
+After inserting one element check wheather it is following heap properties or not.
+
+#2nd method: First create complete binary tree using given list numbers
+Then start heapifying tree : Start from last leaf node
+Modify this method : Start heapify from last internal node.
+
+'''
