c42f75f424465a8dbba66d12889f437f1794dd3d
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-14 16:15:56 +0530

    added tut35 and tut36

---
+++ tut35_intro_Graph.py
@@ -0,0 +1,21 @@
+#=========== Introduction To Graphs Data Strucutre ===============
+
+'''
+-->Graph is a non linear data structure consisiting of nodes and edges
+-->Nodes are also called as vertices 
+--> Tree is a special type of graph that means tree will always be a graph but
+    not all graph will be trees
+
+--> Tree are nothing more then the restricted type of graph.
+--> In Tree we have a special node that is root node , and only one path between any 2 nodes.
+--> In Graph we do not have any special node called root node here all nodes are equal.
+    and between any 2 nodes we have multiple path.
+    Graph can contain cycles , if we start from one node that after traversing few node we reach
+    to same node
+
+--> Graphs are used in Google map and Gps : To find shortest fast from one place to another.
+--> And to show some recomendation , and google search algorithem also use graph
+
+--> In computer science graph are used to show the flow of computations.
+
+'''
---
+++ tut36_Graph_Type_terms.py
@@ -0,0 +1,23 @@
+#============= Type Of Graphs =========
+
+'''
+Based on direction of the edges  we have two type in Graph:
+
+1- Directed Graph : A graph in which all the edges are uni-directional
+
+2- Undirected Graph: A graph in which all the edges are bi-directional
+
+Based on weight we have two type of Graph:
+
+1-Weighted Graph : A graph in which each edge is assigned with some weight/cost/value.
+
+2-Unweighted Graph: A graph where there is no value or weight associated with the edge.
+
+Based on graph contain cycle or not we have two type of graph:
+
+1-Cyclic Graph : if contain cycle then cyclic graph
+   
+2-Acyclic Graph : if not contain cycle then Acyclic Graph.
+     # Tree is a acyclic graph.
+
+'''
