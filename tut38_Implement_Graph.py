a33cfc6adde8f09293dfa6dd59bddf5abae2f7ef
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-16 21:22:41 +0530

    Added add_edges function

--- tut38_Implement_Graph.py
+++ tut38_Implement_Graph.py
@@ -10,7 +10,7 @@
 4-Function to add an edge using adjadency list representation.
 
 '''
-#1-Function to add a node using adjacency matrix representation.
+#1-===========Function to add a node using adjacency matrix representation.=========================
 
 # This code is for undirected Graph 
 # In python to repersent values in matrix form we use nested list.
@@ -21,7 +21,7 @@
 nodes = []
 adjacency_matrix_graph=[]
 
-def add_node(node):
+def add_node(node): # this function work fine for all graph weather directed undirected weighted or unweighted
     temp=[]
     # for i in nodes:
     #     if i == node :
@@ -51,6 +51,54 @@ add_node("G")
 add_node("H")
 add_node("K")
 
+print("Before Adding Edge \n")
 print(nodes)
 print(adjacency_matrix_graph)
 print_in_matrix(adjacency_matrix_graph)
+
+#=================== 2-Function to add an edge using adjadency matrix representation.==============
+
+# this is only for undirected and unweighted graph.
+# for weighted graph add cost and where we give one give cost simple.
+# in directed graph we can make directed weighted graph or directed unweighted graph same as undirected graph
+   # here if we give add_edge(A,B) then matrix[A][B] = 1 or cost not matrix[B][A] =1 or cost simple .
+   # it means it directed from A to B not From B to A.
+
+def add_edge(v1,v2,cost): # it take edge as a arguments.
+
+    if v1 and v2 not in nodes:
+        print(" Node is not present")
+    else:
+            
+        # for index,item in enumerate(nodes): # this for loop is for find index of given vertex/nodes.
+        #     if item == v1:
+        #         index_v1 = index
+        #     if item == v2:
+        #         index_v2 = index
+        #     else:
+        #         pass
+        index_v1 = nodes.index(v1)
+        index_v2 = nodes.index(v2)
+
+        adjacency_matrix_graph[index_v1][index_v2] = cost
+        adjacency_matrix_graph[index_v2][index_v1] = cost
+
+        # for i in range(len(adjacency_matrix_graph)):# With this two for loop we access matrix.
+        #     for j in range(len(adjacency_matrix_graph[i])):
+        #         if  i == index_v1 and j == index_v2:
+        #             adjacency_matrix_graph[i][j] = 1
+        #             adjacency_matrix_graph[j][i] = 1
+
+
+print("After Adding Edge \n") 
+
+add_edge("F","G",10)
+add_edge("F","K",5)
+add_edge("F","X",7) # here X is not present but i give X as a input so that i check my code.
+print(nodes)
+print(adjacency_matrix_graph)
+print_in_matrix(adjacency_matrix_graph)
+
+
+
+
