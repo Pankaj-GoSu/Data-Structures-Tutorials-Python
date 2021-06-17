30b549b7e3c4f3c9f5140adcb4169110cdf91a50
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-17 10:08:23 +0530

    Added tut40

---
+++ tut40_Delete_Operation_in_Graph.py
@@ -0,0 +1,21 @@
+# ======== Deletion Operation in Graph ==========
+
+'''
+--> Like add operation here also we do delete node and delete edge operation.
+
+'''
+
+'''
+
+# Delete - Node / Delete - Vertex
+
+delete_node(v) --> delete v it also delete the connection to v.
+# adj_matrix : Delete the Row and column of index v.
+# adj_list : Delete key value pair of v
+             and Check the value od all key in dictionary for v , if it is 
+             present remove that from the list
+
+#Delete Edge
+delete_edge(v1,v2)
+
+'''
