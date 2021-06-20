3fe861b4df83f5e2fe11f5e6a07040e835cbc006
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-21 00:09:45 +0530

    Added Insertion Sort

--- Data Structure and Algorithems by apna college/Sorting in Array.py
+++ Data Structure and Algorithems by apna college/Sorting in Array.py
@@ -5,7 +5,7 @@
 
 '''
 
-# Selection Sort :
+#=============== Selection Sort ============:
 
 '''
 Find the minimum element in unsorted array and swap it with element at begining.
@@ -58,4 +58,82 @@ def selection_sort2(array): # this code is not for selection sort becoz here in 
 selection_sort2(array1)
 print("After sorting array : method 2")
 print(array1)
+
+
+#===== Bubble Sort ==========
+
+'''
+Reapetedly swap two adjacent elements if they are in wrong order.
+
+Hum ise bubble sort isliye kahte h qnki jo humara maximum element hota h wo bubble ki trh upar aa jata h.
+'''
+
+def Bubble_sort(array,n):
+    i = 0
+    j = 1
+    if n == 1:
+        return array
+    else:    
+        while(j < n): # here n is decreasing such that we not check last element after 1st sorting or after completion of one loop.
+            if array[j]<array[i]:# here it is repeatedly swap when wrong order.
+                array[i],array[j] = array[j],array[i]
+            i = i + 1
+            j = j + 1
+        Bubble_sort(array,n-1)
+
+
+array2 = [12,45,23,51,19,8]
+Bubble_sort(array2,len(array2))
+print(f"Bubble sort result : {array2}")
+
+
+#======= Insertion Sort ==========
+
+'''
+Insert an element from unsorted array to its correct position in sorted array.
+
+array- [12,45,23,51,19,8]
+step-1 [12,45,23,51,19,8]
+step-2 [12,23,45,51,19,8]
+step-3 [12,23,45,51,19,8]
+step-4 [12,19,23,45,51,8]
+step-5 [8,12,19,23,45,51]
+
+'''
+# ======== This is sorting method by IBH ==========
+# array3 = [12,45,23,51,19,8]
+
+# def Insert(array,temp):
+#     if len(array) == 0 or array[-1]<temp:
+#         array.append(temp)
+#     else:
+#         temp1 = array[-1]
+#         array.pop()
+#         Insert(array,temp)
+#         array.append(temp1)
+
+
+# def Insertion_Sort(array):
+
+#     if len(array) == 1:
+#         return array
+#     else:
+#         temp = array[-1]
+#         array.pop()
+#         Insertion_Sort(array)
+#         Insert(array,temp)
+
+# Insertion_Sort(array3)
+# print(f"Insertion sort result : {array3}")
+
+array3 = [12,45,23,51,19,8]
+
+def Insertion_sort(array):
+    i = 0
+    if len(array) == 1:
+        return array
+    while (i+1 < len(array)):
+        if array[i] > array[i+1]:
+            
+        i = i+1
 
