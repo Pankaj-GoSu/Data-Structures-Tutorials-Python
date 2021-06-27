751fc210cb1d1341c913e2b13c84ee45997a197e
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-27 20:30:55 +0530

    Added Infix to Prefix

--- Data Structure and Algorithems by apna college/Infix to Prefix.py
+++ Data Structure and Algorithems by apna college/Infix to Prefix.py
@@ -1,6 +1,4 @@
-# ========= Infix to Postfix using stack ==============
-
-# first we implement stack
+#=========== Infix to Prefix =========
 
 class Stack():
 
@@ -50,62 +48,53 @@ class Stack():
         else:
             print(f"Your stack is {self.stack}")
 
-#========= Implementing Infix to Postfix ========
+# ========= Implementing Infix to Prefix =================
 
-    def infix_to_postfix(self,list1):
+    def infix_to_prefix(self,list1):# it give us result from left to right but result is write this output in note book from right to left.
 
-        for i in range(len(list1)):
+        for i in range(len(list1)-1,-1,-1):
 
             if type(list1[i]) == int:
-                print(list1[i],end ="")
-                
+                print(list1[i],end="")
             else:
-                if list1[i] == "(":
-                    self.push("(")
+                if list1[i] == ")":
+                    self.push(")")
                 elif list1[i] == "/":
                     self.push("/")
                 elif list1[i] == "*":
                     self.push("*")
-                
                 elif list1[i] == "+" or list1[i] == "-":
-                    
-                    if  len(self.stack) == 0: 
+                    if len(self.stack) == 0:
                         self.push(list1[i])
-
                     else:
-                        if self.stack[-1] == "+" or self.stack[-1] == "-" :
+                        if self.stack[-1] == "+" or self.stack[-1] == "-":
                             self.push(list1[i])
                         else:
                             a = self.pop()
-                            while a != "+" and a !="-":
-                                print(a, end = "")
+                            while a != "+" and a != "-":
+                                print(a,end = "")
                                 a = self.pop()
                             self.push(a)
                             self.push(list1[i])
-                        
-
-                elif list1[i] == ")":
+                
+                elif list1[i] == "(":
                     while True:
-                        a = self.pop()
-                        if a=="(":
-                            break
+                        if self.stack[-1] != ")":
+                            a = self.pop()
+                            print(a,end ="")
                         else:
-                            print(a, end = "")
+                            break
         while True:
             if len(self.stack) != 0:
                 a = self.pop()
                 print(a, end="")
             else:
-                break
-                   
-                    
-                        
-st = Stack(15)
+                break                    
+
+
+
+st = Stack(20)
 
 list1 = [5,"-",2,"*",3,"+",6]
-list2 = []
-st.infix_to_postfix(list1)
-# st.print_stack()
-print()
-# print(len(st.stack))
-# st.print_stack()
+
+st.infix_to_prefix(list1)
