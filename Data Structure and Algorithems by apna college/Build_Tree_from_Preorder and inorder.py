01ebbed2340b02d132c326a6815caad50e3779a2
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-30 22:16:42 +0530

    added postorder traversal

--- Data Structure and Algorithems by apna college/Build_Tree_from_Preorder and inorder.py
+++ Data Structure and Algorithems by apna college/Build_Tree_from_Preorder and inorder.py
@@ -1,69 +1,54 @@
 # ======== build Tree from Preorder and Inorder ==========
 
-from typing import OrderedDict
-
-
-
-class Tree:
+class Node:
 
     def __init__(self,data):
         self.data = data
         self.lchild = None
         self.rchild = None
 
-        
+def buildtree(preorder,inorder,st,end):
+    global idx
+    if idx == len(preorder):
+        return None
+    
+    if (st>end):
+        return None
+    
+    new_node = Node(preorder[idx])
+    
+    index1 = inorder.index(preorder[idx])
+    
+    idx = idx +  1
+    
+    if st == end:
+        return new_node
 
-    def tree_constructor(self,preorder,inorder):
+    new_node.lchild = buildtree(preorder,inorder,st,index1-1)
+    new_node.rchild = buildtree(preorder,inorder,index1+1,end)
 
-        self.data = preorder[0]
-        index1 = inorder.index(preorder[0])
-        index2 = inorder.index(preorder[0])
-        preorder1 = preorder[0:index1+1]
-        preorder2 = preorder[index1+1:len(preorder)+1]
-        preorder1.pop(0)
-        while len(preorder1) != 0:
-            index_x = inorder.index(preorder1[0])
-            if index_x < index2:
-                if self.lchild:
-                    self.lchild.data = preorder1[0]
-                else: 
-                    self.lchild = Tree(preorder1[0])    
-            else :
-                if self.rchild:
-                    self.rchild.data = preorder1[0]
-                else:
-                    self.rchild = Tree(preorder1[0])
-            index1 = index_x
-            preorder1.pop(0)
-        
-        preorder2.pop(0)
-        while len(preorder2) != 0:
-            index_y = inorder.index(preorder2[0])
-            if index_y < index2:
-                if self.lchild:
-                    self.lchild.data = preorder2[0]
-                else: 
-                    self.lchild = Tree(preorder2[0])    
-            else :
-                if self.rchild:
-                    self.rchild.data = preorder2[0]
-                else:
-                    self.rchild = Tree(preorder2[0])
-            index2 = index_y
-            preorder2.pop(0)
-        
-        
-    def preorder(self):
-        print(self.data,end = " ")
-        if self.lchild:
-            self.lchild.preorder()
-        if self.rchild:
-            self.rchild.preorder()
+    return new_node
 
-preorder =[1,2,4,3,5]
+def prInorder(node):
+ 
+    if (node == None):
+        return
+         
+    prInorder(node.lchild)
+    print(node.data, end = " ")
+    prInorder(node.rchild)
+
+def postorder(node):
+    if node == None:
+        return
+    postorder(node.lchild)
+    postorder(node.rchild)
+    print(node.data,end =" ")
+idx = 0
+preorder = [1,2,4,3,5]
 inorder = [4,2,1,5,3]
-# index = inorder.index(preorder[0])
-# print(index)
-tree1 = Tree(None)
-tree1.tree_constructor(preorder,inorder)
-tree1.preorder()
+
+root = buildtree(preorder,inorder,0,5)
+prInorder(root)
+print()
+postorder(root)
