4c3cda0f34260e11a3a191a0d2f78cedca52f6a4
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-03 20:40:27 +0530

    Added left view of binary tree

--- Data Structure and Algorithems by apna college/BinaryTree_Left_view.py
+++ Data Structure and Algorithems by apna college/BinaryTree_Left_view.py
@@ -1,9 +1,9 @@
-# ============== Right view of a binary tree ==============
+#====== Left view of a Binary Tree ========
 
 '''
-It means what nodes are visible to us when we see from right side.
+it means what nodes are visible to us when we see from left side.
 --> It is done by level by level so here we do level wise traversal
---> In each level we print right most node only of each level.
+--> In each level we print left most node only of each level.
 
 '''
 
@@ -29,22 +29,14 @@ class Binary_Tree:
             else:
                 self.rchild = Binary_Tree(data)
     
-    def preorder(self):
+    def level_order_traversal(self):
 
-        print(self.data,end = " ")
-        if self.lchild:
-            self.lchild.preorder()
-        if self.rchild:
-            self.rchild.preorder()
+        queue = [self] # it contain top most node or current node (not value).
+        next_queue = [] # it conatains childs of current nodes.
+        level = [] # in level list we put or value of nodes level wise.
+        result = [] # this store final list of lists of every level
 
-    def level_order_Traversal(self):
-
-        queue = [self] # it store top most node (not value)
-        next_queue = [] # it store next elements of our tree or childs of current node
-        level = [] # it store level wise value.
-        result = [] # it store final value in list of list format.
-
-        while queue != []:
+        while queue != []: # it run till queue not become empty.
 
             for nodes in queue:
                 level.append(nodes.data)
@@ -52,11 +44,12 @@ class Binary_Tree:
                     next_queue.append(nodes.lchild)
                 if nodes.rchild:
                     next_queue.append(nodes.rchild)
-            queue = next_queue
-            next_queue = []
-            result.append(level)
-            level = []
+            queue = next_queue # here we update our queue
+            next_queue = [] # reinitialize
+            result.append(level) # here we store list of value in each level
+            level = [] # reinitialize
         return result
+
 tree1 = Binary_Tree(None)
 
 tree1.insert(10)
@@ -72,11 +65,10 @@ tree1.insert(13)
 tree1.insert(12)
 tree1.insert(11)
 
-list_lvl =tree1.level_order_Traversal()
-
-print(list_lvl)
-print("right view of tree is :")
-right_view = []
+list_lvl = tree1.level_order_traversal()
+print("Left view of tree is:")
+left_view = []
 for lists in list_lvl:
-    right_view.append(lists[-1])
-print(right_view)
+    left_view.append(lists[0])
+
+print(left_view)
