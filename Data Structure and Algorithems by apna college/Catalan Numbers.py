2ee3c388ab6765bb3a2b6f3ccbd1cc4d47ce4f26
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-07 17:57:43 +0530

    Added Catalan Number

---
+++ Data Structure and Algorithems by apna college/Catalan Numbers.py
@@ -0,0 +1,26 @@
+#======== Catalan Numbers Application ========
+
+'''
+Catalan numbers are a sequence of natural numbers that occur in various counting problems,
+often involving recursively defined objects.
+
+'''
+
+
+#======= Code for find nth Catalan Number =========
+
+def nth_Catalan(n):
+    result = 0
+    
+    if n <= 1:
+        return 1
+    
+    for i in range(n):
+        result = result + nth_Catalan(i)*nth_Catalan(n-i-1)
+
+    return result
+
+
+
+for i in range(10):
+    print(nth_Catalan(i) , end = " ")
